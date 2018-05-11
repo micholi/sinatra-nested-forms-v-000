510aabedc3cb864947a1351a8613df3af1e614b8
Michelle Olivieri <michelleo72@gmail.com>
2018-05-11 21:19:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,6 +13,9 @@ module FormsLab
 
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
     end
 
   end
